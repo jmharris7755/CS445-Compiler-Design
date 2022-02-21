e2b7c68eeb3d55d57842c5a58777e2ea6e93dd52
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2022-02-21 00:48:02 -0800

    Update semantic.h

--- Homework 3/semantic.h
+++ Homework 3/semantic.h
@@ -7,10 +7,10 @@
 
 SymbolTable returnSymbolTable();
 void semanticAnalysis(TreeNode *t, int& errors, int& warnings);
-void analyze(TreeNode *t, int& numErrors, int&numWarnings);
-void checkDecl(TreeNode *t, int& numErrors, int& numWarnings);
-void checkStmt(TreeNode *t, int& numErrors, int& numWarnings);
-void checkExp(TreeNode *t, int& numErrors, int& numWarnings);
+void analyze(TreeNode *t, int& nErrors, int& nWarnings);
+void checkDecl(TreeNode *t, int& nErrors, int& nWarnings);
+void checkStmt(TreeNode *t, int& nErrors, int& nWarnings);
+void checkExp(TreeNode *t, int& nErrors, int& nWarnings);
 void getExpTypes(const char* string, bool isBinary, bool &unaryErrors, ExpType &left, ExpType &right, ExpType &rightT);
 char* conExpType(ExpType type);
 void printError(int errCode, int linenum, int explaineno, char* s1, char* s2, char* s3, double d);
