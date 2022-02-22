8ac0f9d439b0b1663f51c80fce0af2b111e32172
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2022-02-21 17:51:59 -0800

    Update semantic.h

--- Homework 3/semantic.h
+++ Homework 3/semantic.h
@@ -14,5 +14,6 @@ void checkExp(TreeNode *t, int& nErrors, int& nWarnings);
 void getExpTypes(const char* string, bool isBinary, bool &unaryErrors, ExpType &left, ExpType &right, ExpType &rightT);
 char* conExpType(ExpType type);
 void printError(int errCode, int linenum, int explaineno, char* s1, char* s2, char* s3, double d);
+void checkArrays(TreeNode *tree);
 
 #endif
